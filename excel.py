1e6d9c412163c4e8f49c7d5d63cf89a5b8f25166
vinod <vinod.a.naik@gmail.com>
2014-03-18 01:57:37 +0530

    Python code to return column number of the excel when the header string is provided

---
+++ excel.py
@@ -0,0 +1,29 @@
+import sys
+import string
+
+def excel(input):
+    n=len(input)
+    hasht={}
+    i=0;
+    letters=string.lowercase[:26]
+    input=input.lower()
+
+    while i!= 26:
+        hasht[letters[i]]=i+1
+        i+=1
+        
+    no=0
+    while(n!=0):
+        no=no+(pow(26,(n-1))*hasht[input[-n]])
+        n-=1
+
+    return no
+
+  
+if __name__=='__main__':
+    if len(sys.argv) <2:
+        print "Provide command line arguments\n"
+        exit(0)
+    print excel(sys.argv[1])
+
+    
