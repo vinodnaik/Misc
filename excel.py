ec94eafd2feb15c67e368d7244a46c454d70bf61
vinod <vinod.a.naik@gmail.com>
2014-04-02 02:20:17 +0530

    Refined the code. Added for loops for pythonic way

--- excel.py
+++ excel.py
@@ -4,17 +4,17 @@ import string
 def excel(input):
     n=len(input)
     hasht={}
-    i=0;
+    idx=1;
     letters=string.lowercase[:26]
     input=input.lower()
 
-    while i!= 26:
-        hasht[letters[i]]=i+1
-        i+=1
+    for letter in letters:
+        hasht[letter]=idx
+        idx+=1
         
     no=0
-    while(n!=0):
-        no=no+(pow(26,(n-1))*hasht[input[-n]])
+    for inp in input:
+        no=no+(pow(26,n-1)*hasht[inp])
         n-=1
 
     return no
@@ -24,7 +24,8 @@ if __name__=='__main__':
     if len(sys.argv) <2:
         print "Provide command line arguments\n"
         exit(0)
-    elif any(char.isdigit() for char in sys.argv[1]):
+    #elif any(char.isdigit() for char in sys.argv[1]):
+    elif not sys.argv[1].isdigit():
         print "Column numbers cannot contain numbers\nProvide valid column number"
         exit(0)
     print excel(sys.argv[1])
